baaf5b1aaf3284e641214e7f1dc366cd35a3d2df
Glitch (eclipsio) <none>
2020-06-13 16:17:45 +0000

    🚇📻 Checkpoint ./bundle.js:13763590/2440

--- bundle.js
+++ bundle.js
@@ -3112,8 +3112,8 @@
                     e = w();
                 w();
                 var c = [ //You can change the tips lol
-                    ["Tip: You can view and edit your keybinds in the options menu.", "Tip: You can play on mobile by just going to arras.io on your phone!"],
-                    ["Tip: You can have the shield and health bar be separated by going to the options menu.", "Tip: If Arras is having a low frame rate, you can try enabling low graphics in the options menu.", "Tip: You can make traps rounded with the classic trap setting in the options menu."
+                    ["Tip: You can view and edit your keybinds in the options menu.", "Tip: You can play on mobile by just going to Eclipsio.glitch.me on your phone!"],
+                    ["Tip: You can have the shield and health bar be separated by going to the options menu.", "Tip: If Eclipse is having a low frame rate, you can try enabling low graphics in the options menu.", "Tip: You can make traps rounded with the classic trap setting in the options menu."
                     ],
                     ["Teaming in FFA or FFA Maze is frowned upon, but when taken to the extremes, you can be punished.", "Witch hunting is when you continuously target someone and follow them. This is frowned upon, but when taken to the extremes, you can be punished.", "Multiboxing is when you use a script to control multiple tanks at the same time. This is considered CHEATING and will result in a ban."]
                 ];
